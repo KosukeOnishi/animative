31e919cd82785bfb8fc3aabf73e62a900a8d9478
KosukeOnishi <63177660+KosukeOnishi@users.noreply.github.com>
2020-06-09 12:45:45 +0900

    scale

--- lib/screens/home_screen/home_screen.dart
+++ lib/screens/home_screen/home_screen.dart
@@ -5,27 +5,60 @@ class HomeScreen extends StatefulWidget {
   _HomeScreenState createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 500),
+    );
+    _controller.forward();
+  }
+
+  void _toggle() {
+    _controller.isDismissed
+        ? _controller.forward()
+        : _controller.reverse();
+  }
+
   @override
   Widget build(BuildContext context) {
+    print(_controller.value);
     return Scaffold(
-      body: Container(
-        width: double.infinity,
-        height: double.infinity,
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            stops: [0.0, 0.2, 1.0],
-            colors: <Color>[
-              Colors.blue[900],
-              Colors.blue[700],
-              Colors.blue[50],
-            ],
+      body: GestureDetector(
+        onDoubleTap: _toggle,
+        child: Container(
+          width: double.infinity,
+          height: double.infinity,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              stops: [0.0, 0.2, 1.0],
+              colors: <Color>[
+                Colors.blue[900],
+                Colors.blue[700],
+                Colors.blue[50],
+              ],
+            ),
           ),
-        ),
-        child: Center(
-          child: Text('test'),
+          child: Center(
+            child: AnimatedBuilder(
+              animation: _controller,
+              builder: (context, child) {
+                return Transform.scale(
+                  scale: _controller.value,
+                  child: Text(
+                    'scale',
+                    style: TextStyle(fontSize: 40),
+                  ),
+                );
+              },
+            ),
+          ),
         ),
       ),
     );
